3232a638b9798f0b4e6c6150a80aaf98f5f365db
Mohamad Arif Hidayat <moharifhidayat7@gmail.com>
2024-11-11 00:45:27 +0700

    prompt input math expression

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,30 @@
 use std::io::stdin;
 
 fn main() {
-    println!("Please enter the first number: ");
-    let mut first_input = String::new();
-    stdin().read_line(&mut first_input).unwrap();
-    let first_input: i32 = first_input.trim().parse().unwrap();
+    println!("Please enter the math operation (e.g. 1+2, 2*3): ");
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+    let input = input.trim();
 
-    println!("Please enter the second number: ");
-    let mut second_input = String::new();
-    stdin().read_line(&mut second_input).unwrap();
-    let second_input: i32 = second_input.trim().parse().unwrap();
+    let (first_input, operation, second_input) = parse_input(input);
 
-    println!("Choose the operation: ");
-    println!("1. Add");
-    println!("2. Subtract");
-    println!("3. Multiply");
-    println!("4. Divide");
-
-    let mut operation = String::new();
-    stdin().read_line(&mut operation).unwrap();
-    let operation: i32 = operation.trim().parse().unwrap();
-    
     match operation {
-        1 => add(first_input, second_input),
-        2 => subtract(first_input, second_input),
-        3 => multiply(first_input, second_input),
-        4 => divide(first_input, second_input),
+        '+' => add(first_input, second_input),
+        '-' => subtract(first_input, second_input),
+        '*' => multiply(first_input, second_input),
+        '/' => divide(first_input, second_input),
         _ => println!("Invalid operation")
     }
     main();
+}
+
+fn parse_input(input: &str) -> (i32, char, i32) {
+    let mut chars = input.chars();
+    let first_input: i32 = chars.by_ref().take_while(|c| c.is_digit(10)).collect::<String>().parse().unwrap();
+    let operation = chars.next().unwrap();
+    let second_input: i32 = chars.skip_while(|c| c.is_whitespace()).collect::<String>().parse().unwrap();
+
+    (first_input, operation, second_input)
 }
 
 fn add(first_input: i32, second_input: i32) {
